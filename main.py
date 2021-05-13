824bc2ec8674ee26d3731497b2e990fe0a39c80f
Dennis Kenneth <dennyify123@gmail.com>
2021-05-13 14:41:52 +0000

    1st commit

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+PIRPLE ASSIGNMENT IN PYTHON ABOUT FAVOURITE SONG
+"""
+#MY FAVOURITE SONG'S NAME
+songName = "What's Next"
+
+#ARTIST'S NAME
+artist = "Drake"
+
+#GENRE THAT THE SONGG BELONGS TO
+genre = "Rap & Hip-Hop"
+
+#DURATION OF THE SONG IN SECONDS
+durationInSeconds = 200
+
+#THE YEAR OF RELEASE OF THE SONG
+releaseYear = 2021
+
+"""
+NAME OF THE PROJECT THE SONG WAS PART OF
+"""
+projectName = "Scary Hours II"
+
+"""
+NAME OF INDIVIDUALS INVOLVED IN WRITING THE SONG
+"""
+songWriters = "Aubrey Graham, Jonathan D. Priester, Maneesh Bidaye"
+
+"""
+POSITION WHICH THE SONG PICKED AT ON THE MUSIC CHARTS
+"""
+peakPosition = 1
+
+#PRODUCERS NAME
+producer = "Noah '40' Shabib"
+
+"""
+NAME OF RECORD LABEL THE ARTIST IS SIGNED TO
+"""
+recordLabel = "OVO Music"
+
+"""
+RATING GIVEN TO THE SONG BY CRITICS
+"""
+rating = 4.5
+
+print(artist)
+print(genre)
+print(durationInSeconds)
+print(releaseYear)
+print(projectName)
+print(songWriters)
+print(peakPosition)
+print(producer)
+print(recordLabel)
+print(rating)
